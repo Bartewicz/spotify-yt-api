e28d5d14d145d7222259c0fea9f2cbd8c4051f87
bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-10-15 19:56:14 +0200

    Create Auth component for login button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react'
 // Components
+import Auth from './components/Auth'
 import Dashboard from './components/Dashboard'
 import Footer from './components/Footer'
 import Header from './components/Header'
 // UI
 import './App.css'
-
-const style = {
-  authWrapper: {
-    padding: '50px',
-    height: '100%',
-    textAlign: 'center'
-  },
-  loginButton: {
-    cursor: 'pointer',
-    height: '50px',
-    padding: '0 15px',
-    borderRadius: '50px',
-    border: '0',
-    backgroundColor: '#1db954',
-    color: '#fff',
-    fontWeight: 'bold'
-  }
-}
 
 class App extends Component {
   state = {
@@ -37,26 +20,7 @@ class App extends Component {
           {
             this.state.user ?
               <Dashboard user={this.state.user}/> :
-              <div
-                style={style.authWrapper}
-              >
-                <h1>
-                  {'Welcome!'}
-                </h1>
-                <h2>
-                  {'Just log in with your Spotify account to start using the app.'}
-                </h2>
-                <button
-                  onClick={() => {
-                    if (window.confirm('Do you want to login?'))
-                      this.setState({ user: 'Darling' })
-                  }}
-                  style={style.loginButton}
-                  value={'login-true'}
-                >
-                  {'Login with Spotify'}
-                </button>
-              </div>
+              <Auth />
           }
         </main>
         <Footer />
